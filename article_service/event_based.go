7b95689feef06429fd746d39fa842f77ea5c5252
Andre Prasetya <andre.prasetya@gmail.com>
2021-03-27 17:38:03 +0700

    add find one event bus

--- article_service/event_based.go
+++ article_service/event_based.go
@@ -1,9 +1,16 @@
 package article_service
 
 import (
+	"context"
+	"time"
+
 	"github.com/ztrue/shutdown"
 	"gopkg.in/thejerf/suture.v4"
 )
+
+var supervisor *suture.Supervisor
+var serviceToken suture.ServiceToken
+var ebc ArticleEventBusClient
 
 type FindAllRequest struct {
 	resultChannel chan<- FindAllResponse
@@ -14,53 +21,97 @@ type FindAllResponse struct {
 	err      error
 }
 
+type FindOneRequest struct {
+	id            int
+	resultChannel chan<- FindOneResponse
+}
+
+type FindOneResponse struct {
+	article *Article
+	err     error
+}
+
 type ArticleEventBus struct {
+	ctx                   context.Context
+	cancel                context.CancelFunc
 	findAllRequestChannel chan FindAllRequest
+	findOneRequestChannel chan FindOneRequest
 	articleService        ArticleService
 }
 
 func (eb *ArticleEventBus) Serve() {
-	var req FindAllRequest
+	var far FindAllRequest
+	var foner FindOneRequest
 	for {
 		select {
-		case req = <-eb.findAllRequestChannel:
+		case <-eb.ctx.Done():
+			return
+		case far = <-eb.findAllRequestChannel:
 			articles, err := eb.articleService.FindAll()
 			findAllResponse := FindAllResponse{
 				articles: articles,
 				err:      err,
 			}
-			req.resultChannel <- findAllResponse
+			far.resultChannel <- findAllResponse
+		case foner = <-eb.findOneRequestChannel:
+			article, err := eb.articleService.FindOne(foner.id)
+			findOneResponse := FindOneResponse{
+				article: article,
+				err:     err,
+			}
+			foner.resultChannel <- findOneResponse
 		}
 	}
 }
 
 func (eb *ArticleEventBus) Stop() {
-
+	eb.cancel()
 }
 
 func (eb *ArticleEventBus) FindAllRequestChannel() chan<- FindAllRequest {
 	return eb.findAllRequestChannel
 }
 
+func (eb *ArticleEventBus) FindOneRequestChannel() chan<- FindOneRequest {
+	return eb.findOneRequestChannel
+}
+
 type ArticleEventBusClient interface {
 	FindAllRequestChannel() chan<- FindAllRequest
+	FindOneRequestChannel() chan<- FindOneRequest
 }
 
 func ConfigureEventBus(test bool) (ArticleEventBusClient, error) {
-	supervisor := suture.NewSimple("ArticleEventBusSupervisor")
-	eb := ArticleEventBus{
-		findAllRequestChannel: make(chan FindAllRequest, 1),
-		articleService:        Configure(test),
+	if supervisor == nil {
+		supervisor = suture.NewSimple("ArticleEventBusSupervisor")
+		go supervisor.ServeBackground()
+	}
+	if ebc == nil {
+		ctx, cancel := context.WithCancel(context.Background())
+		eb := ArticleEventBus{
+			ctx:                   ctx,
+			cancel:                cancel,
+			findAllRequestChannel: make(chan FindAllRequest, 1),
+			findOneRequestChannel: make(chan FindOneRequest, 1),
+			articleService:        Configure(test),
+		}
+
+		service := &eb
+		serviceToken = supervisor.Add(service)
+		shutdown.Add(func() {
+			supervisor.Stop()
+		})
+		ebc = &eb
 	}
 
-	service := &eb
-	supervisor.Add(service)
-	go supervisor.ServeBackground()
-	shutdown.Add(func() {
-		supervisor.Stop()
-	})
-	var ebc ArticleEventBusClient
-	ebc = &eb
-
 	return ebc, nil
+}
+
+func ShutdownEventBus() error {
+	err := supervisor.RemoveAndWait(serviceToken, 1*time.Second)
+	if err != nil {
+		return err
+	}
+	ebc = nil
+	return nil
 }
--- article_service/event_based_test.go
+++ article_service/event_based_test.go
@@ -12,6 +12,33 @@ func TestConfigureEventBus(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
+	if eventBus.FindAllRequestChannel() == nil {
+		t.Errorf("returned event bus has nil findAllRequestChannel")
+	}
+	if eventBus.FindOneRequestChannel() == nil {
+		t.Errorf("returned event bus has nil findOneRequestChannel")
+	}
+}
+
+func TestDoubleConfigureEventBusThenResultShouldBeTheSame(t *testing.T) {
+	firstEventBus, err1 := ConfigureEventBus(true)
+	if err1 != nil {
+		t.Error(err1)
+	}
+	secondEventBus, err2 := ConfigureEventBus(true)
+	if err2 != nil {
+		t.Error(err2)
+	}
+	if firstEventBus != secondEventBus {
+		t.Errorf("event bus not identical")
+	}
+}
+
+func TestSendingFindAllRequest(t *testing.T) {
+	eventBus, err := ConfigureEventBus(true)
+	if err != nil {
+		t.Error(err)
+	}
 	resultChannel := make(chan FindAllResponse)
 	defer close(resultChannel)
 	req := FindAllRequest{
@@ -38,5 +65,86 @@ func TestConfigureEventBus(t *testing.T) {
 	wg.Wait()
 	if findAllResponse == nil {
 		t.Errorf("No response in 500 ms")
+	}
+}
+
+func TestSendingFindOneRequest(t *testing.T) {
+	eventBus, err := ConfigureEventBus(true)
+	if err != nil {
+		t.Error(err)
+	}
+	resultChannel := make(chan FindOneResponse)
+	defer close(resultChannel)
+	req := FindOneRequest{
+		resultChannel: resultChannel,
+	}
+	eventBus.FindOneRequestChannel() <- req
+	var findOneResponse *FindOneResponse
+	var wg sync.WaitGroup
+	wg.Add(1)
+	fmt.Println("Spawning request")
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-time.After(500 * time.Millisecond):
+				fmt.Println("timeout")
+				return
+			case res := <-resultChannel:
+				findOneResponse = &res
+				return
+			}
+		}
+	}()
+	wg.Wait()
+	if findOneResponse == nil {
+		t.Errorf("No response in 500 ms")
+	}
+}
+
+func TestManualShutdownEventBus(t *testing.T) {
+	eventBus, err := ConfigureEventBus(true)
+	if err != nil {
+		t.Error(err)
+	}
+	resultChannel := make(chan FindAllResponse)
+	defer close(resultChannel)
+	err = ShutdownEventBus()
+	if err != nil {
+		t.Error(err)
+	}
+	req := FindAllRequest{
+		resultChannel: resultChannel,
+	}
+	eventBus.FindAllRequestChannel() <- req
+	var findAllResponse *FindAllResponse
+	var wg sync.WaitGroup
+	wg.Add(1)
+	fmt.Println("Spawning request")
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-time.After(500 * time.Millisecond):
+				fmt.Println("timeout")
+				return
+			case res := <-resultChannel:
+				findAllResponse = &res
+				return
+			}
+		}
+	}()
+	wg.Wait()
+	if findAllResponse != nil {
+		t.Errorf("There is still a response even though we have shut it down")
+	}
+}
+
+func TestShutdownAndReconfigure(t *testing.T) {
+	firstEventBus, _ := ConfigureEventBus(true)
+	ShutdownEventBus()
+	secondEventBus, _ := ConfigureEventBus(true)
+	if firstEventBus == secondEventBus {
+		t.Errorf("second event bus is identical to before shutdown")
 	}
 }
